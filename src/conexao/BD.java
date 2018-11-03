5bb05dc135ba95194fc225c3e0d1062116c02fcb
Fernando Almeida <dealmeida.fernando96@yahoo.com.br>
2018-11-03 02:56:01 -0300

    feat: implements functions for BD

--- src/conexao/BD.java
+++ src/conexao/BD.java
@@ -4,16 +4,18 @@ package conexao;
 import java.sql.*;
 
 public class BD {
-
+static Connection conexao;
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         conecta();
+        executa("insert into job(title, description, salary, state, city) values('t','t',2,'sp','sp')");
+        lista();
+        desconecta();
         
     }
     public static void conecta(){
-        Connection conexao;
         String driver = "com.mysql.jdbc.Driver";
         String url = "jdbc:mysql://localhost:3306/vagas";
         String login = "root";
@@ -25,5 +27,22 @@ public class BD {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    public static void executa(String sql) throws Exception{
+        Statement st = null;
+        st = conexao.createStatement();
+        st.executeUpdate(sql);
+    }
+    public static void lista() throws Exception{
+        Statement st;
+        st = conexao.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * from job");
+        System.out.println("-----------------------");
+        while(rs.next()){
+            System.out.println("Cod: " + rs.getString(1));
+        }
+}
+    public static void desconecta() throws Exception {
+        conexao.close();
     }
 }
